c01b6d875f30076a724c1b84da9e2cdaf5feabe1
great12theworld <88846533+great12theworld@users.noreply.github.com>
2026-05-07 10:08:58 -0400

    worked on js for the input detection.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let playerName = null
-let seed = null
-function startGame() {
-    
-}
+let seed = null //for game mechanics later on
+
+let inputStart = 0;
+
+const terminal = document.getElementById('terminal')
+
+terminal.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        const fullText = terminal.innerText;
+        const newText = fullText.substring(inputStart).trim();
+        inputStart = fullText.length;
+    }
+})
