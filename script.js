635eaf06b3f328afffd15ba5736d0087e070f24b
great12theworld <88846533+great12theworld@users.noreply.github.com>
2026-05-07 22:41:34 -0400

    added code to make cursor move to end and for the eventual text to self type.

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ terminal.addEventListener('keydown', (event) => {
     }
     
 })
+
+function moveCursorToEnd() {
+    terminal.focus();
+
+    const range = document.createRange();
+    range.selectNodeContents(terminal);
+    range.collapse(false);
+    
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+}
+async function typeText(text,speed = 20) {
+    for (let i = 0; i < text.length; i++){
+        terminal.innerText += text[i];
+        moveCursorToEnd();
+        await new Promise(resolve => setTimeout(resolve, speed));
+    }
+}
