5f45f9b004f6f7ff09bcfe17b99deeb7855ab9ef
great12theworld <88846533+great12theworld@users.noreply.github.com>
2026-05-09 16:30:28 -0400

    I fixed problems with overflow on x with the css, the js file got minor tweaks I think, I belive I forgot to push yesterday, Working Edition

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ let gameStart = false;
 
 let commandCount = 3;
 
+let playerInelligence = 0
+let playerStrength = 0
+let playerAgility = 0
+let playerLuck = 0
+
+
 // for Starting Select systems:
-let stateStartingSelect = 0;
-let points = 10;
+let stateStartingSelect = "unstarted";
+let statPoints = 15;
 let costIntelligence = 1;
 let costStrength = 1;
 let costAgility = 1;
@@ -30,6 +36,12 @@ function moveCursorToEnd() {
     selection.addRange(range);
 
 }
+
+function scrollToBottom() {
+    terminal.scrollTop = terminal.scrollHeight;
+}
+
+
 //changed from a DOM system to text content for better input/output handling.
 
 async function typeText(text,speed = 20) {
@@ -41,9 +53,10 @@ async function typeText(text,speed = 20) {
         line.textContent += text[i];
 
         moveCursorToEnd();
+        scrollToBottom();
 
         await new Promise(resolve =>
-            setTimeout(resolve, speed)
+            setTimeout(resolve, 0)
         );
     }
 }
@@ -52,6 +65,7 @@ function addLineBreak(text) {
     const line = document.createElement('div');
     line.textContent = text;
     terminal.appendChild(line);
+    scrollToBottom();
 }
 
 
@@ -60,16 +74,16 @@ terminal.addEventListener('beforeinput', async (event) => {
         event.preventDefault();
         const fullText = terminal.innerText;
         const newText = fullText.substring(inputStart).trim();
-        console.log(newText);
         
         if (newText.toLowerCase() === "help") {
             // Handle help command
             if (commandCount === 3) {
                 await typeText("Available commands: help, start, clear", 30);
             } else if (commandCount === 4) {
-                await typeText("Available commands: help, clear ", 30);
+                await typeText("Available commands: help, clear, loadstats, checkstats ", 30);
             }
         }
+
         if (newText.toLowerCase() === "start" && commandCount === 3) {
             gameStart = true;
         }
@@ -82,18 +96,83 @@ terminal.addEventListener('beforeinput', async (event) => {
             await typeText("I'm gonna patch you in now.",50);
             await typeText("Good luck!",50);
             await typeText("Loading",50);
-            await typeText("...",500);
-            await typeText("Done!",50);
-            await startingSelect();
+            await typeText("...", 500);
+            await typeText("Done!", 50);
             gameStart = false;
             commandCount ++;
         }
+        
+        if (newText.toLowerCase() === "loadstats" && stateStartingSelect === "unstarted" && commandCount == 4){
+            startingSelect()
+            stateStartingSelect = "running"
+        }
+        if (stateStartingSelect === "running" && newText.toLowerCase() === "1"){
+            if (statPoints >= 1){
+                statPoints = statPoints - 1;
+                playerInelligence ++;                        
+                await typeText("1 point added to Intelligence")
+                await typeText("Points remaining: " + statPoints)
+                await typeText("please select another number: (1), (2), (3), (4)")
+                moveCursorToEnd()
+            } 
+        
+        }else if (stateStartingSelect === "running" && newText.toLowerCase() === "2"){
+            if (statPoints >= 1){
+                statPoints = statPoints - 1;
+                playerStrength ++;                        
+                await typeText("1 point added to Strength")
+                await typeText("Points remaining: " + statPoints)
+                await typeText("please select another number: (1), (2), (3), (4)")
+                moveCursorToEnd()
+            }
 
-        addLineBreak(">");
-        inputStart = terminal.innerText.length;
-        moveCursorToEnd();
 
-    }
+        } else if (stateStartingSelect === "running" && newText.toLowerCase() === "3"){
+            if (statPoints >= 1){
+                statPoints = statPoints - 1;
+                playerAgility ++;
+                await typeText("1 point added to Agility")
+                await typeText("Points remaining: " + statPoints)
+                await typeText("please select another number: (1), (2), (3), (4)")
+                moveCursorToEnd()
+            }
+            
+        
+        
+        
+        } else if (stateStartingSelect === "running" && newText.toLowerCase() === "4"){
+            if (statPoints >= 1){
+                statPoints = statPoints - 1;
+                playerLuck ++;                        
+                await typeText("1 point added to Luck")
+                await typeText("Points remaining: " + statPoints)
+                await typeText("please select another number: (1), (2), (3), (4)")
+                moveCursorToEnd()
+            }
+
+        } 
+        if (stateStartingSelect === "running" && statPoints < 1){
+            await typeText ("You have now used all of your points")
+            await typeText ("Your stats are the following:")
+            await typeText ("Intelligence: " + playerInelligence)
+            await typeText ("Strength: " + playerStrength)
+            await typeText ("Agility: " + playerAgility)
+            await typeText ("Luck: " + playerLuck)
+            
+            stateStartingSelect = "ended"
+
+        }
+
+        if (newText.toLowerCase() === "checkstats" && commandCount == 4){
+            checkStats();
+        }
+            
+
+        endWrite();
+
+
+
+    } //end of enter listening
     
 })
 
@@ -103,12 +182,36 @@ inputStart = terminal.innerText.length;
 
 
 //actual systems
-async function startingSelect(){
+async function startingSelect(){ //only printing text for system.
     await typeText("Welcome, you have been successfully loaded in", 50);
     await typeText("You are now ready to begin your setup.", 50);
-    await typeText("Please choose how you want to split your points:", 50);
-    await typeText("1. Intelligence" + costIntelligence, 50);
-    await typeText("2. Strength" + costStrength, 50);
-    await typeText("3. Agility" + costAgility, 50);
-    await typeText("4. Luck Cost:" + costLuck, 50);
+    await typeText("Please choose how you want to split your 15 points:", 50);
+    await typeText("1. Intelligence Cost: " + costIntelligence, 50);
+    await typeText("2. Strength Cost: " + costStrength, 50);
+    await typeText("3. Agility Cost: " + costAgility, 50);
+    await typeText("4. Luck Cost: " + costLuck, 50);
+    await typeText("Type a number: (1), (2), (3), (4)",50)
+
+    addLineBreak(">");
+    inputStart = terminal.innerText.length;
+    moveCursorToEnd();
+
+}
+
+async function checkStats() {
+    await typeText ("Your stats are the following:")
+    await typeText ("Intelligence: " + playerInelligence)
+    await typeText ("Strength: " + playerStrength)
+    await typeText ("Agility: " + playerAgility)
+    await typeText ("Luck: " + playerLuck)
+
+    addLineBreak(">");
+    inputStart = terminal.innerText.length;        
+    moveCursorToEnd();
+}
+
+async function endWrite() {
+    addLineBreak(">");
+    inputStart = terminal.innerText.length;
+    moveCursorToEnd();
 }
