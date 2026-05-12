5bc372924fbc1f9bdf3f31c60706efe8a4473858
great12theworld <88846533+great12theworld@users.noreply.github.com>
2026-05-12 14:21:36 -0400

    got playground working, still need to clean and comb through playground to ensure refined code. Working edition.

--- playground_script.js
+++ playground_script.js
@@ -1,6 +1,7 @@
 //let playerName = null
 //let seed = null //for game mechanics later on
 
+import {storyOpening} from "./story.js"
 
 let inputStart = 0;
 
@@ -110,7 +111,7 @@ async function typeText(text,speed = 20) {
 }
 
 function addLineBreak(text) {
-    const line = document.createElement('div');
+    const line = document.createElement('div'); 
     line.textContent = text;
     terminal.appendChild(line);
     scrollToBottom();
@@ -176,7 +177,7 @@ terminal.addEventListener('beforeinput', async (event) => {
         }
         
         if (newText.toLowerCase() === "loadstats" && stateStartingSelect === "unstarted" && commandCount == 4){
-            startingSelect()
+            await startingSelect()
             stateStartingSelect = "running"
         }
         if (stateStartingSelect === "running" && newText.toLowerCase() === "1"){
@@ -237,14 +238,21 @@ terminal.addEventListener('beforeinput', async (event) => {
         }
 
         if (openingStoryState === "unstarted" && unlockedCommands.includes("startstory") && newText.toLowerCase() === "startstory"){
-            storyOpening();
+            await storyOpening();
+            // addLineBreak("> ");
+            // inputStart = terminal.innerText.length;        
+            // moveCursorToEnd();
+            unlockedCommands.push("callsomeone")
+            unlockedCommands.push("checkmoney")
+            unlockedCommands.push("checkstats")
+            unlockedCommands.push("checkrelations")
             commandCount = 6;
         }
         if (newText.toLowerCase() === "checkmoney" && unlockedCommands.includes("checkmoney")){
-            CheckMoney();
+            await CheckMoney();
         }
         if (newText.toLowerCase() === "checkstats" && unlockedCommands.includes("checkstats")){
-            checkStats();
+            await checkStats();
         }
 
         //checkrelations\add more characters stuff
@@ -271,9 +279,9 @@ terminal.addEventListener('beforeinput', async (event) => {
         }
         
 
-        addLineBreak("> ");
-        inputStart = terminal.innerText.length;
-        moveCursorToEnd();
+        // addLineBreak("> ");
+        // inputStart = terminal.innerText.length;
+        // moveCursorToEnd();
         
 
 
@@ -283,8 +291,10 @@ terminal.addEventListener('beforeinput', async (event) => {
 })
 
 //spin up terminal
+//spin up terminal
 terminal.textContent = "> ";
 inputStart = terminal.innerText.length;
+moveCursorToEnd();
 
 
 //actual systems
@@ -298,9 +308,7 @@ async function startingSelect(){ //only printing text for system.
     await typeText("4. Luck Cost: " + costLuck);
     await typeText("Type a number: (1), (2), (3), (4)")
 
-    addLineBreak("> ");
-    inputStart = terminal.innerText.length;
-    moveCursorToEnd();
+
 
 }
 
@@ -311,78 +319,15 @@ async function checkStats() {
     await typeText ("Agility: " + playerAgility)
     await typeText ("Luck: " + playerLuck)
 
-    addLineBreak("> ");
-    inputStart = terminal.innerText.length;
-    moveCursorToEnd();
 }
 
-async function endWrite() {
-    addLineBreak("> ");
-    inputStart = terminal.innerText.length;
-    moveCursorToEnd();
-}
-
-
-
-async function storyOpening(){
-    await typeText("'What is wrong with you today!' - Your mom is yelling at you.  ", 30)
-    await typeText("You dont know what to say.", 30)
-    await typeText("'Why are you doing this to me!' - She said this with tears brimming collapse against her...", 60)
-    await typeText("frail,", 75)
-    await typeText("heavy eyelids.", 75)
-    await typeText("You wake up.", 50)
-    await typeText("-----", 40)
-    await typeText("-----", 40)
-    await typeText("-----", 40)
-    await typeText("That fragrant morning smell assimilates into the insides of your nose. You cut open your last apple.")
-    await typeText("It smells wonderful. It smells like the memory of flowers blossoming and freshly cut grass.")
-    await typeText("Everything feels so, nice. Like no matter what you did, it would be okay.")
-    await typeText("'Hi mom!' - You say, seeing her in the hallway of your apartment on the 42nd floor of the biggest apartment rental in the district")
-    await typeText("'Everything alright?' - She says, stopping by while carrying two huge boxes.")
-    await typeText("The white hue of the boxes reminds you of your sister for some reason.")
-    await typeText("-----")
-    await typeText("'Yeah, no I just had a bad dream is all.' - You notice labeling on the boxes for a-")
-    await typeText(" 'THE GOLDBERG FOUNDATION??' - You yell out.", 40)
-    await typeText("-----")
-    await typeText("'I dont see what's so wrong about that?' - The Goldberg Foundation is one of the largest corporations to claim all resources from the government.")
-    await typeText("After the government started giving out what little resources remained, split evenly across all people of course, large groups of people banded together.")
-    await typeText("The Goldburg Foundation was the largest of all. They had managed to amass over 2.4 Million chips worth of food, clothing, water and more. Nothing was left.")
-    await typeText("They operated by getting desperate people without families to give everything they had for a chance to earn more.")
-    await typeText("Those who performed well, recived chips in the form of food and clothing.")
-    await typeText("Those who did otherwise...", 50)
-    await typeText("-----")
-    await typeText(" -- You dont respond.")
-    await typeText("Your mom continues walking down the hall. You hear the thud of the boxes against the kitchen countertop.")
-    await typeText("You look out your window which stares downward into the streets below and upwards to the airy clouds.")
-    await typeText("-----")
-    await typeText("At the train station:   ")
-    await typeText("'Bye sweetie!! I'll see you next time you visit alright!!' -- You smile from inside the train as you make your way home.")
-    await typeText("You have some things to do if you intend to survive.")
-    await typeText("-----",70)
-    await typeText("SYSTEM MSG: You will officially begin your story in the city of Lurilin.")
-    await typeText("Your story will be shaped by the decisions you make.")
-    await typeText("You can do things from calling people, meeting new ones, working. Even climbing the ranks of the Goldburg Foundation if you wish.")
-    await typeText("You can make your story all about money, love, revenge, or power.")
-    await typeText("Each one will be determined by numerous built in factors, each one having different levels of difficulty.")
-    await typeText("Each ending carving itself in its own way.")
-    await typeText("As of now only one ending can be achieved at one point")
-    await typeText("More help options have been unlocked, use them")
-    await typeText("As you proceed through the game certain commands will lock themselves, or SHOULD lock themselves")
-    await typeText("Ensuring that only one path out of the four can be attained.")
-    await typeText("--END MSG--");
-    addLineBreak("> ");
-    inputStart = terminal.innerText.length;        
-    moveCursorToEnd();
-    unlockedCommands.push("callsomeone")
-    unlockedCommands.push("checkmoney")
-    unlockedCommands.push("checkstats")
-    unlockedCommands.push("checkrelations")
-}
+// async function endWrite() {
+//     addLineBreak("> ");
+//     inputStart = terminal.innerText.length;
+//     moveCursorToEnd();
+// }
 
 async function CheckMoney() {
     await typeText("Your current chip count is:")
     await typeText(CurrentMoney +" Chips")
-    addLineBreak("> ")
-    inputStart = terminal.innerText.length;
-    moveCursorToEnd();
 }
---
+++ story.js
@@ -0,0 +1,54 @@
+import { 
+    typeText,
+ } from "./script.js";
+
+
+export async function storyOpening(){
+    await typeText("'What is wrong with you today!' - Your mom is yelling at you.  ", 30)
+    await typeText("You dont know what to say.", 30)
+    await typeText("'Why are you doing this to me!' - She said this with tears brimming collapse against her...", 60)
+    await typeText("frail,", 75)
+    await typeText("heavy eyelids.", 75)
+    await typeText("You wake up.", 50)
+    await typeText("-----", 40)
+    await typeText("-----", 40)
+    await typeText("-----", 40)
+    await typeText("That fragrant morning smell assimilates into the insides of your nose. You cut open your last apple.")
+    await typeText("It smells wonderful. It smells like the memory of flowers blossoming and freshly cut grass.")
+    await typeText("Everything feels so, nice. Like no matter what you did, it would be okay.")
+    await typeText("'Hi mom!' - You say, seeing her in the hallway of your apartment on the 42nd floor of the biggest apartment rental in the district")
+    await typeText("'Everything alright?' - She says, stopping by while carrying two huge boxes.")
+    await typeText("The white hue of the boxes reminds you of your sister for some reason.")
+    await typeText("-----")
+    await typeText("'Yeah, no I just had a bad dream is all.' - You notice labeling on the boxes for a-")
+    await typeText(" 'THE GOLDBERG FOUNDATION??' - You yell out.", 40)
+    await typeText("-----")
+    await typeText("'I dont see what's so wrong about that?' - The Goldberg Foundation is one of the largest corporations to claim all resources from the government.")
+    await typeText("After the government started giving out what little resources remained, split evenly across all people of course, large groups of people banded together.")
+    await typeText("The Goldburg Foundation was the largest of all. They had managed to amass over 2.4 Million chips worth of food, clothing, water and more. Nothing was left.")
+    await typeText("They operated by getting desperate people without families to give everything they had for a chance to earn more.")
+    await typeText("Those who performed well, recived chips in the form of food and clothing.")
+    await typeText("Those who did otherwise...", 50)
+    await typeText("-----")
+    await typeText(" -- You dont respond.")
+    await typeText("Your mom continues walking down the hall. You hear the thud of the boxes against the kitchen countertop.")
+    await typeText("You look out your window which stares downward into the streets below and upwards to the airy clouds.")
+    await typeText("-----")
+    await typeText("At the train station:   ")
+    await typeText("'Bye sweetie!! I'll see you next time you visit alright!!' -- You smile from inside the train as you make your way home.")
+    await typeText("You have some things to do if you intend to survive.")
+    await typeText("-----",70)
+    await typeText("SYSTEM MSG: You will officially begin your story in the city of Lurilin.")
+    await typeText("Your story will be shaped by the decisions you make.")
+    await typeText("You can do things from calling people, meeting new ones, working. Even climbing the ranks of the Goldburg Foundation if you wish.")
+    await typeText("You can make your story all about money, love, revenge, or power.")
+    await typeText("Each one will be determined by numerous built in factors, each one having different levels of difficulty.")
+    await typeText("Each ending carving itself in its own way.")
+    await typeText("As of now only one ending can be achieved at one point")
+    await typeText("More help options have been unlocked, use them")
+    await typeText("As you proceed through the game certain commands will lock themselves, or SHOULD lock themselves")
+    await typeText("Ensuring that only one path out of the four can be attained.")
+    await typeText("--END MSG--");
+
+    let commandCount = 6;
+ }
